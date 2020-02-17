12160416dac22909786df30d5ff74482101cd803
Elton Rodrigues <elton.leite@multilaser.com.br>
2020-02-17 11:16:35 -0300

    Criado a rota para receber pagamentos.

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,6 +1,18 @@
 module.exports = (app) => {
   app.get('/pagamentos', (req, res) => {
-    console.log('Hahahaha');
+    console.log('Lista pagamentos');
+
     res.send('Ok');
+  });
+
+  app.post('/pagamentos/pagamento', (req, res) => {
+    const pagamento = req.body;
+
+    console.log('Processando um pagamento...');
+
+    pagamento.status = 'Criado';
+    pagamento.data = new Date();
+
+    res.send(pagamento);
   });
 };
